1ad28140ae5935816d4b5393d9abab6ff658816b
浩天 <xeefei@gmail.com>
2025-12-26 20:16:01 +0800

    Update polygon.go

--- app/task/polygon.go
+++ app/task/polygon.go
@@ -14,13 +14,13 @@ func polygonInit() {
 		Network:  conf.Polygon,
 		Endpoint: conf.GetPolygonRpcEndpoint(),
 		Block: block{
-			InitStartOffset: -600,
-			ConfirmedOffset: 40,
+			InitStartOffset: -50,
+			ConfirmedOffset: 15,
 		},
 		blockScanQueue: chanx.NewUnboundedChan[evmBlock](ctx, 30),
 	}
 
 	register(task{callback: pol.blockDispatch})
-	register(task{callback: pol.blockRoll, duration: time.Second * 5})
-	register(task{callback: pol.tradeConfirmHandle, duration: time.Second * 5})
+	register(task{callback: pol.blockRoll, duration: time.Second * 45})
+	register(task{callback: pol.tradeConfirmHandle, duration: time.Second * 45})
 }
